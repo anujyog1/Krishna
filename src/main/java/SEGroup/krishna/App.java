38d97caf8623181b0105763d17c366fc9facc665
anujyog1 <66674832+anujyog1@users.noreply.github.com>
2024-01-04 12:35:47 +0530

    Update App.java

--- src/main/java/SEGroup/krishna/App.java
+++ src/main/java/SEGroup/krishna/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Krishna hai" );
+        System.out.println( "Hello World!Krishna hai_______ demo for pooling" );
     }
 }
